d42ef0caf8bb5741533619721f57945834106463
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2023-06-08 13:23:20 -0500

    version 1.8.1

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 ?>
     <main class="container seccion">
         <h1>Administrador de Bienes Raices</h1>
-        <a href="/admin/properties/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+        <a href="properties/crear.php" class="boton boton-verde">Nueva Propiedad</a>
     </main>
 <?php
     incluirTemplate('footer');
