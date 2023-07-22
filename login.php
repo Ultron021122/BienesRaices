06753dde38fb969327ac8c953552d6d6296ca873
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2023-07-21 22:21:51 -0500

    version 2.5.1

--- login.php
+++ login.php
@@ -1,24 +1,83 @@
 <?php
-    require 'includes/funciones.php';
-    incluirTemplate('header');
-?>
-    <main class="container seccion contenido-centrado">
-        <h1>Iniciar Sesión</h1>
+// Conexión a la base de datos
+require 'includes/config/database.php';
+$db = conectarDB();
 
-        <form class="formulario">
-            <fieldset>
-                <legend>Credenciales</legend>
+// Autenticar el usuario
+$errores = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
+
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (!$email) {
+        $errores[] = "El email es obligatorio o no es válido";
+    }
+    if (!$password) {
+        $errores[] = "El password es obligatorio";
+    }
+
+    if (empty($errores)) {
+        // Revisar si el usuario existe
+        $query = "SELECT * FROM users WHERE email = '$email'";
+        $resultado = mysqli_query($db, $query);
+
+        if( $resultado->num_rows ) {
+            // Revisar si el password es correcto
+            $usuario = mysqli_fetch_assoc($resultado);
+
+            // Verificar el password
+            $auth = password_verify($password, $usuario['password']);
+            if( $auth ) {
+                // El usuario esta autenticado
+                session_start();
+
+                // Llenar el arreglo de la sesión
+                $_SESSION['usuario'] = $usuario['email'];
+                $_SESSION['login'] = true;
                 
-                <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu E-mail" id="email">
+            } else {
+                $errores[] = "El password es incorrecto";
+            }
+        } else {
+            $errores[] = "El usuario no existe";
+        }
+    }
+}
 
-                <label for="password">Password</label>
-                <input type="password" placeholder="Tu Password" id="password">
-            </fieldset>
+// Incluye el header
+require 'includes/funciones.php';
+incluirTemplate('header');
+?>
+<main class="container seccion contenido-centrado">
+    <h1>Iniciar Sesión</h1>
 
-            <input type="submit" class="boton boton-verde" value="Iniciar sesión">
-        </form>
-    </main>
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+    <form class="formulario" method="post">
+        <fieldset>
+            <legend>Credenciales</legend>
+
+            <label for="email">E-mail</label>
+            <input type="email" name="email" placeholder="Tu E-mail" id="email">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Tu Password" id="password">
+        </fieldset>
+
+        <input type="submit" class="boton boton-verde" value="Iniciar sesión">
+    </form>
+</main>
 <?php
-    incluirTemplate('footer');
+incluirTemplate('footer');
+
+// Cerrar la conexión
+mysqli_close($db);
 ?>
